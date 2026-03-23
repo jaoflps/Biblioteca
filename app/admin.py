e6f1f3691b331339708c8094ea5460fa0e14874a
João Felipe <joaofelipeferreira1234@gmail.com>
2026-03-23 08:01:40 -0300

    Add files via upload
    Biblioteca completa com Inline

--- app/admin.py
+++ app/admin.py
@@ -1,14 +1,26 @@
-#Registre seus modelos aqui.
+# Registre seus modelos aqui.
 from django.contrib import admin
-from . models import *
-from django.contrib import admin
+from .models import *
+
 
 admin.site.register(Cidade)
-admin.site.register(Autor)
+# admin.site.register(Autor) 
 admin.site.register(Editora)
 admin.site.register(Leitor)
 admin.site.register(Livro)
 admin.site.register(Genero)
 admin.site.register(Emprestimo)
 
- 
+# Os inlines vem primeiro, pois eles são usados dentro do admin do autor
+class LivroInline(admin.TabularInline):
+    model = Livro
+    extra = 1
+
+# Agora criamos a classe do admin do autor, onde vamos usar o inline para mostrar os livros relacionados a cada autor
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+    inlines = [LivroInline] # Isso faz os livros aparecerem dentro do autor
+
+
+admin.site.register(Autor, AutorAdmin)
